f1c15be8c8a03626bcf0e9bada77ad64fa18b203
Ádám Keserű <keseru.adam@gmail.com>
2023-04-18 12:31:06 +0200

    UI

--- GUIapr18.Frontend/App.xaml.cs
+++ GUIapr18.Frontend/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using GUIapr18.Logic;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +16,30 @@ namespace GUIapr18.Frontend
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        public IServiceProvider Services { get; }
+
+        public App()
+        {
+            Services = ConfigureServices();
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<IMessageLogic, MessageLogic>();
+            services.AddSingleton<IMessenger>(WeakReferenceMessenger.Default);
+            services.AddTransient<MainWindowViewModel>();
+
+            return services.BuildServiceProvider();
+        }
     }
 }
--- GUIapr18.Frontend/MainWindowViewModel.cs
+++ GUIapr18.Frontend/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace GUIapr18.Frontend
 {
@@ -11,9 +12,11 @@ namespace GUIapr18.Frontend
     {
         public RestCollection<Message> Messages { get; set; }
 
+        public ICommand SendMessageCommand { get; set; }
+
         public MainWindowViewModel() 
         {
-            Messages = new RestCollection<Message>("http://localhost:5258/", "Message");
+            Messages = new RestCollection<Message>("http://localhost:5258/", "message");
         }
     }
 }
